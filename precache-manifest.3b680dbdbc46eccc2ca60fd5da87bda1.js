d9409c0e8e17ebd99b4597f7e29ee59123bdc06f
roman <romagladyshev@gmail.com>
2019-11-06 10:47:50 +0600

    3

---
+++ precache-manifest.3b680dbdbc46eccc2ca60fd5da87bda1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93c9a91f24e7dcb0083787095f0cbe9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c31669d88505b041b8a",
+    "url": "/static/css/main.4ed5a0aa.chunk.css"
+  },
+  {
+    "revision": "b3c9174e70494c4b6d00",
+    "url": "/static/js/2.550ea0cd.chunk.js"
+  },
+  {
+    "revision": "9c31669d88505b041b8a",
+    "url": "/static/js/main.b6592019.chunk.js"
+  },
+  {
+    "revision": "53fb5f2a5c362a92d31e",
+    "url": "/static/js/runtime-main.9a6be4de.js"
+  },
+  {
+    "revision": "8a0116d08691b039d775c68618e76a26",
+    "url": "/static/media/backgroundHeader.8a0116d0.jfif"
+  },
+  {
+    "revision": "2542b82cd34747302ce9bcefa1bfc15c",
+    "url": "/static/media/burgerBuilder.2542b82c.png"
+  },
+  {
+    "revision": "03ce6cbd29c4050029b85b96bc350e1d",
+    "url": "/static/media/htmlcss.03ce6cbd.svg"
+  },
+  {
+    "revision": "91455950a8726491e14009682e9bcee3",
+    "url": "/static/media/projectImage.91455950.jpg"
+  },
+  {
+    "revision": "13a8ba6aae7226e7014a5dd096420c20",
+    "url": "/static/media/sql.13a8ba6a.jpg"
+  }
+]);
