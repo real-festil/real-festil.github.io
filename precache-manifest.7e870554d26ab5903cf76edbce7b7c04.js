06773319fd334fdb581764906a5a446f4571e876
roman <romagladyshev@gmail.com>
2019-11-15 00:16:41 +0600

    Updates

---
+++ precache-manifest.7e870554d26ab5903cf76edbce7b7c04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fbbe7306ae9d1bf0994f8cc544dc413",
+    "url": "/shopApp/index.html"
+  },
+  {
+    "revision": "0f43f5d1d7ffc8824d4d",
+    "url": "/shopApp/static/css/2.55521911.chunk.css"
+  },
+  {
+    "revision": "675ebcd0e39af48f6b17",
+    "url": "/shopApp/static/css/main.a15c1319.chunk.css"
+  },
+  {
+    "revision": "0f43f5d1d7ffc8824d4d",
+    "url": "/shopApp/static/js/2.43740d4e.chunk.js"
+  },
+  {
+    "revision": "675ebcd0e39af48f6b17",
+    "url": "/shopApp/static/js/main.63010f45.chunk.js"
+  },
+  {
+    "revision": "e7bbbfc98e4ad7903df3",
+    "url": "/shopApp/static/js/runtime-main.bfa07760.js"
+  }
+]);
